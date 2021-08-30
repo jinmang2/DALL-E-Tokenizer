c4b76cf06eb1fd895047a51c0eee2e853ef714a7
MyungHoon Jin <jinmang2@gmail.com>
2021-08-31 03:06:47 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+  
+from setuptools import setup
+
+def parse_requirements(filename):
+	lines = (line.strip() for line in open(filename))
+	return [line for line in lines if line and not line.startswith("#")]
+
+setup(
+    name='DALL-E-Tokenizer',
+    version='0.1',
+    description='Huggingface package for the discrete VAE usded for DALL-E.',
+    url='https://github.com/jinmang2/DALL-E-Tokenizer',
+    author='MyungHoon Jin',
+    author_email='jinmang2@gmail.com',
+    packages=['dall_e_tok'],
+    install_requires=parse_requirements('requirements.txt'),
+    zip_safe=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
